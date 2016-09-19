76678a9edf7e48e1a3bad949f862b3da78be578e
cstrase <cstrase@gmail.com>
2016-09-19 18:42:16 -0500

    final commit for signup

--- main.py
+++ main.py
@@ -14,34 +14,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import re
 import webapp2
+import cgi
+
+
+
 
 form="""
 <form method="post">
 <label>
     User Name:
-    <input type="text" name="username" value="%(username)s">
+    <input type="text" name="username" value="%(username)s" required>
 </label>
+<div style="color: red">%(error_username)s</div>
 <br>
 <label>
     Password:
-    <input type="text" name="password" value="%(password)s">
+    <input type="password" name="password" value="%(password)s" required>
 </label>
+<div style="color: red">%(error_password)s</div>
 <br>
 <label>
     Verify Password:
-    <input type="text" name="veryify_password" value="%(veryify_password)s">
+    <input type="password" name="verify_password" value="%(verify_password)s" required>
 </label>
+<div style="color: red">%(error_verify)s</div>
 <br>
 <label>
 Email:
-    <input type="text" name="email" value="%(email)s">
+    <input type="text" name="email" value="%(email)s" required>
 </label>
+<div style="color: red">%(error_email)s</div>
 <br>
 <input type="submit">
 </form>
 """
+#reg expressions to check for validity of user input
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(user_name):
+        return user_name and USER_RE.match(user_name)
 
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(user_password):
+        return PASSWORD_RE.match(user_password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(user_email):
+        return not user_email or EMAIL_RE.match(user_email)
 
 
 
@@ -49,16 +69,58 @@ Email:
 
 
 class MainHandler(webapp2.RequestHandler):
-    def form_Writer(self, username="", password="", veryify_password="", email=""):
+    def form_Writer(self, username="", error_username="", password="", error_password="",  verify_password="", error_verify="", email="", error_email=""):
         self.response.out.write(form % {"username": username,
                                     "password": password,
-                                    "veryify_password" : password,
-                                    "email" : email})
-
-    def post(self)
+                                    "verify_password": verify_password,
+                                    "email": email,
+                                    "error_password": error_password,
+                                    "error_username": error_username,
+                                    "error_verify": error_verify,
+                                    "error_email": error_email})
 
     def get(self):
         self.form_Writer()
+
+        #variables that store user input
+    def post(self):
+        have_error = False
+        user_name = self.request.get('username')
+        user_password = self.request.get('password')
+        user_verify = self.request.get('verify_password')
+        user_email = self.request.get('email')
+        #variables to check validity
+        username_error = ""
+        password_error = ""
+        verify_error = ""
+        email_error = ""
+
+        if not valid_username(user_name):
+                username = user_name
+                username_error="Please provide a correct username"
+                have_error = True
+
+        if not valid_email(user_email):
+                email = user_email
+                email_error="Please provide a correct email address"
+                have_error = True
+
+
+        if not valid_password(user_password):
+                password = user_password
+                password_error= "Provide a valid password"
+                have_error = True
+
+
+        if  user_verify != user_password:
+                verify_error="Your passwords did not match"
+                have_error = True
+
+        if have_error:
+            self.form_Writer(username = user_name, error_username = username_error, error_password = password_error, error_verify = verify_error, email = user_email, error_email = email_error)
+
+        else:
+            self.response.write("Great job " + user_name + "!")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
